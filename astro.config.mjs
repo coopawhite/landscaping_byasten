f6769357af4fb1811041eecf562ea6880b149099
coopawhite <cooperjwhite0@gmail.com>
2026-04-04 13:51:08 +1100

    chore: set GitHub Pages site URL and Formspree endpoint

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,9 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
-// TODO: Replace these with your actual GitHub username and repo name.
-// site: 'https://YOUR_USERNAME.github.io'
-// base: '/YOUR_REPO_NAME'
-// If this is a user/org site (repo named username.github.io), set base: '/'
 export default defineConfig({
   output: 'static',
-  site: 'https://YOUR_USERNAME.github.io',
+  site: 'https://coopawhite.github.io',
   base: '/landscaping_byasten',
   integrations: [tailwind()],
 });
